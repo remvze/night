e70b7e822389c57860096d4fd572a025262637ef
MAZE <remvze@proton.me>
2024-10-06 19:55:47 +0330

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<div align="center">
+  <h2>Night</h2>
+  <p>Dark web design inspiration.</p>
+  <a href="https://night.mvze.net/">Visit <strong>Night</strong></a> | <a href="https://buymeacoffee.com/remvze">Buy Me a Coffee</a>
+</div>
---
+++ src/data/websites.ts
@@ -0,0 +1,58 @@
+/**
+ * From Old to New
+ */
+export const websites: Array<{
+  address: string;
+  date: string;
+  name: string;
+  url: string;
+}> = [
+  {
+    address: 'nazhamid.com',
+    date: '2024-09-30',
+    name: 'Naz Hamid',
+    url: 'https://nazhamid.com/',
+  },
+  {
+    address: '2dots.xyz',
+    date: '2024-09-30',
+    name: '2dots',
+    url: 'https://www.2dots.xyz/',
+  },
+  {
+    address: 'tiagorangel.com',
+    date: '2024-09-30',
+    name: 'Tiago Rangel',
+    url: 'https://tiagorangel.com/',
+  },
+  {
+    address: 'andrewtarkan.webflow.io',
+    date: '2024-9-30',
+    name: 'Andrew Tarkan',
+    url: 'https://andrewtarkan.webflow.io/',
+  },
+  {
+    address: 'charliedeets.com',
+    date: '2024-9-30',
+    name: 'Charlie Deets',
+    url: 'https://charliedeets.com/',
+  },
+  {
+    address: 'denismoulin.com',
+    date: '2024-9-30',
+    name: 'Denis Moulin',
+    url: 'https://www.denismoulin.com/',
+  },
+  {
+    address: 'once.com',
+    date: '2024-9-30',
+    name: 'Once',
+    url: 'https://once.com/',
+  },
+  {
+    address: 'reaper.is',
+    date: '2024-9-30',
+    name: 'reaper',
+    url: 'https://reaper.is/',
+  },
+];
