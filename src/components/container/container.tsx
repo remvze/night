14797d863fb065e8acb1d6e6cc6d74048a58c38c
MAZE <remvze@proton.me>
2024-10-06 21:18:35 +0330

    style: make hero full width

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -4,11 +4,20 @@ import styles from './container.module.css';
 
 interface ContainerProps {
   children: React.ReactNode;
+  fullWidth?: boolean;
   wide?: boolean;
 }
 
-export function Container({ children, wide }: ContainerProps) {
+export function Container({ children, fullWidth, wide }: ContainerProps) {
   return (
-    <div className={cn(styles.container, wide && styles.wide)}>{children}</div>
+    <div
+      className={cn(
+        styles.container,
+        wide && styles.wide,
+        fullWidth && styles.fullwidth,
+      )}
+    >
+      {children}
+    </div>
   );
 }
