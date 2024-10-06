c0fe7e33cbdc5087bfe6ca07492828d666d5cccb
MAZE <remvze@proton.me>
2024-10-06 20:47:19 +0330

    fix: correct dates

--- src/data/websites.ts
+++ src/data/websites.ts
@@ -10,21 +10,21 @@ export const websites: Array<{
 }> = [
   {
     address: 'evervault.com',
-    date: '2024-09-30',
+    date: '2024-10-06',
     name: 'Evervault',
     screenshot: '/images/screenshots/evervault.com.webp',
     url: 'https://evervault.com/',
   },
   {
     address: 'dimension.dev',
-    date: '2024-09-30',
+    date: '2024-10-06',
     name: 'Dimension',
     screenshot: '/images/screenshots/dimension.dev.webp',
     url: 'https://dimension.dev/',
   },
   {
     address: 'blockwallet.io',
-    date: '2024-09-30',
+    date: '2024-10-06',
     name: 'BlockWallet',
     screenshot: '/images/screenshots/blockwallet.io.webp',
     url: 'https://blockwallet.io/',
